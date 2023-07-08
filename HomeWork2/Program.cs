8960b525a864e2d36a5c2f3100c5e3d34b9ff73c
Anton Tishchenko <AntonTishchenk0@yandex.ru>
2023-07-08 02:30:37 +0200

    Add HomeWork2

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Task 1. Напишите программу, которая принимает на вход трёхзначное число 
+//         и на выходе показывает вторую цифру этого числа.
+
+int ShowSecNum(int number) 
+{
+    int hundrers = number / 10; 
+    int SecondNumber = hundrers % 10;
+    return SecondNumber;
+}
+
+int ShowSecNum1(int number1)
+{
+    int units = number1 % 100;
+    int SecondNumber = units / 10;
+    return SecondNumber;
+}
+
+int randNumber = new Random().Next(100, 1000);
+int newNumber = ShowSecNum(randNumber);
+int randNumber1 = new Random().Next(100, 1000);
+int newNumber1 = ShowSecNum1(randNumber1);
+
+Console.WriteLine($"The new random number a {randNumber}, and the second digit is {newNumber}.");
+Console.WriteLine($"The new random number a {randNumber1}, and the second digit is {newNumber1}.");
+*/
