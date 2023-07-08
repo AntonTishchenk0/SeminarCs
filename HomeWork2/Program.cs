538a98ea377a028ff1b949deb910b59390549fa6
Anton Tishchenko <AntonTishchenk0@yandex.ru>
2023-07-08 04:00:54 +0200

    Add Task 3

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -28,8 +28,39 @@ Console.WriteLine($"The new random number a {randNumber1}, and the second digit 
 
 int OutputThirdDigit(int number)
 {
-    int  = 
+    
 }
 
-// Task 3. Напишите программу, которая принимает на вход цифру, 
+// 458465095 / 10 = 45846509 |
+// 458465095 / 100 = 4584650 |
+// 458465095 / 1000 = 458465 |
+// 458465095 / 10000 = 45846 |
+// 458465095 / 100000 = 4584 |
+// 458465095 / 1000000 = 458 |
+// 458465095 / 10000000 = 45
+// 458465095 / 100000000 = 4
+
+// 458465095 % 10 = 5
+// 458465095 % 100 = 95
+// 458465095 % 1000 = 095
+// 458465095 % 10000 = 5095
+// 458465095 % 100000 = 65095
+// 458465095 % 1000000 = 465095
+// 458465095 % 10000000 = 8465095   |
+// 458465095 % 100000000 = 58465095 |
+
+/* Task 3. Напишите программу, которая принимает на вход цифру, 
 // обозначающую день недели, и проверяет, является ли этот день выходным.
+
+bool WeekNumber(int number)
+{
+    if (number == 6) return true;
+    if (number == 7) return true;
+    else return false;
+}
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = WeekNumber(number);
+Console.Write(result);
+*/
