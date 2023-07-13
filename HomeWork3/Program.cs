e9cd9fa31d63213df1b9bf0a3b94189914938aaa
Anton Tishchenko <AntonTishchenk0@yandex.ru>
2023-07-13 20:05:23 +0200

    Add HomeWork3

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Task 1. Напишите программу, которая принимает на вход число и проверяет, 
+// является ли оно палиндромом.
+
+bool Palindrome(int palid)
+{
+    int origNum = palid;
+    int reverseNum = 0;
+
+    while (palid > 0)
+    {
+        int digit = palid % 10;
+        reverseNum = reverseNum * 10 + digit;
+        palid = palid / 10;
+    }
+    return origNum == reverseNum;
+}
+
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+bool res = Palindrome(num);
+Console.Write(res);
+*/
