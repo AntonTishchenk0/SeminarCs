0f0fb7bdc91e35305e16ad24c12d13a21060e8cd
Anton Tishchenko <AntonTishchenk0@yandex.ru>
2023-07-18 21:19:26 +0200

    Finish Seminar5

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -47,11 +47,142 @@ int negativeSum = GetNegativeSum(myArray);
 ShowArray(myArray);
 Console.WriteLine("Sum of negatives is: " + negativeSum); */
 
-// Task 2. Напишите программу замена элементов массива: 
+/* Task 2. Напишите программу замена элементов массива: 
 // положительные элементы замените на соответствующие отрицательные, и наоборот.
 
-// Task 3. Задайте массив. Напишите программу, которая определяет, 
+int[] CreateRandArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] NegativeArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+    return array;
+}
+
+Console.Write("Input a length of array: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandArray(length, min, max);
+ShowArray(myArray);
+Console.WriteLine();
+int[] negMyArray = NegativeArray(myArray);
+ShowArray(negMyArray);*/
+
+/* Task 3. Задайте массив. Напишите программу, которая определяет, 
 // присутствует ли заданное число в массиве.
 
-// Task 4. Задайте одномерный массив из m случайных чисел. 
+int[] CreateRandArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+bool IsPresent(int[] array, int numb)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (numb == array[i]) 
+        {
+        return true;
+        }
+    }
+    return false;
+}
+
+Console.Write("Input a length of array: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandArray(length, min, max);
+ShowArray(myArray);
+
+Console.Write("Input number to be checking: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Result is: " + IsPresent(myArray, numb)); */
+
+/* Task 4. Задайте одномерный массив из m случайных чисел. 
 // Найдите количество элементов массива, значения которых лежат в отрезке [a,b].
+
+int[] CreateRandArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int CountArr(int[] array, int a, int b)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= a && array[i] <= b) count++;
+    }
+    return count;
+}
+
+Console.Write("Input a length of array: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandArray(length, min, max);
+ShowArray(myArray);
+
+Console.Write("Input a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int count = CountArr(myArray, a, b);
+Console.Write("Result is: " + count);*/
