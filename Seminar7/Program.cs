7c380a375d210c0639ee48d0e49c479a0ad12998
Anton Tishchenko <AntonTishchenk0@yandex.ru>
2023-07-25 19:37:19 +0200

    Add Seminar7

---
+++ Seminar7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+/* Task 1.
+
+int[,] CreateRandom2dArray(int rows, int colums, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, colums];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < colums; j++)
+            array[i,j] = new Random().Next(minValue, maxValue +1);
+        
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Input a number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number of colums: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min positive value: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max positive value: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(rows, colums, minValue, maxValue);
+Show2dArray(myArray);
+*/
+
