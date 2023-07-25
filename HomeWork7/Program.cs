b5883ad9e765aa8973bd4cd0e9cdf04b21515332
Anton Tishchenko <AntonTishchenk0@yandex.ru>
2023-07-26 01:09:22 +0200

    add HomeWork7

---
+++ HomeWork7/Program.cs
@@ -0,0 +1,58 @@
+﻿/* Task 1. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreateRandom2dArray(int rows, int colums, int minValue, int maxValue)
+{
+    double[,] array = new double[rows, colums];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < colums; j++)  
+            array[i,j] = Math.Round(new Random().NextDouble(), 1);
+    return array;
+}
+
+void Show2dArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Input a number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number of colums: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min value: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max value: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+double[,] myArray = CreateRandom2dArray(rows, colums, minValue, maxValue);
+Show2dArray(myArray);*/
+
+// Task 2. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> числа с такими индексами в массиве нет
+
+
+
+// Task 3. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
