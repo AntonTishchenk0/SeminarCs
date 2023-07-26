6f9279f2219910890652bd467b675471d6df0484
Anton Tishchenko <AntonTishchenk0@yandex.ru>
2023-07-26 07:23:45 +0200

    add Task3 in HomeWork7

--- HomeWork7/Program.cs
+++ HomeWork7/Program.cs
@@ -104,4 +104,56 @@ ElementValue(myArray, i, j);*/
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
+int[,] CreateRandom2dArray(int rows, int colums, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, colums];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < colums; j++)
+            array[i,j] = new Random().Next(minValue, maxValue +1);
+        
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+double[,] Average(int[,] array)
+{
+    double[,] avg = new double[1, array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i,j];
+        }
+        avg[0, j] = sum / array.GetLength(0);
+    }
+    return avg;
+ 
+}
+
+Console.Write("Input a number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number of colums: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min positive value: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max positive value: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(rows, colums, minValue, maxValue);
+Show2dArray(myArray);
+
+double[,] avgArray = Average(myArray);
+Console.Write(avgArray);
 
