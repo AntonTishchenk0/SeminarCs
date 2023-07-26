5761d2d2b8e75070789d6e325be865fb622fce40
Anton Tishchenko <AntonTishchenk0@yandex.ru>
2023-07-26 06:03:04 +0200

    add Task2 in HomeWork7

--- HomeWork7/Program.cs
+++ HomeWork7/Program.cs
@@ -37,15 +37,64 @@ int maxValue = Convert.ToInt32(Console.ReadLine());
 double[,] myArray = CreateRandom2dArray(rows, colums, minValue, maxValue);
 Show2dArray(myArray);*/
 
-// Task 2. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+/* Task 2. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
 // и возвращает значение этого элемента или же указание, что такого элемента нет.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
-// 1 7 -> числа с такими индексами в массиве нет
+// 1 7 -> Числа с такими индексами в массиве нет
 
+int[,] CreateRandom2dArray(int rows, int colums, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, colums];
 
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < colums; j++)
+            array[i,j] = new Random().Next(minValue, maxValue +1);
+        
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+void ElementValue(int[,] array, int i, int j)
+{
+    if(i >= 0 && i < array.GetLength(0) && j >= 0 &&
+    j < array.GetLength(1))
+    {
+        int element = array[i, j];
+        Console.WriteLine($"Element value: {element}");
+    }
+    else Console.WriteLine("There is no such element.");
+}
+
+Console.Write("Input a number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number of colums: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min positive value: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max positive value: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер строки: ");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца: ");
+int j = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(rows, colums, minValue, maxValue);
+Show2dArray(myArray);
+
+ElementValue(myArray, i, j);*/
 
 // Task 3. Задайте двумерный массив из целых чисел. 
 // Найдите среднее арифметическое элементов в каждом столбце.
