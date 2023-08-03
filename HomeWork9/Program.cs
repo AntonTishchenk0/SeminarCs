fd8fcef87112722c21aad6209feb348e8c754434
Anton Tishchenko <AntonTishchenk0@yandex.ru>
2023-08-03 14:58:58 +0200

    add Task3

--- HomeWork9/Program.cs
+++ HomeWork9/Program.cs
@@ -1,4 +1,5 @@
-﻿/* Task 1. Задайте значение N. Напишите программу, 
+﻿using System;
+/* Task 1. Задайте значение N. Напишите программу, 
 // которая выведет все натуральные числа в промежутке от N до 1. 
 // Выполнить с помощью рекурсии.
 
@@ -29,3 +30,17 @@ Console.WriteLine(SumNaturalElements(numb1, numb2));*/
 
 // Task 3. Напишите программу вычисления функции Аккермана с помощью рекурсии. 
 // Даны два неотрицательных числа m и n.
+
+int Accerman(int m, int n)
+{
+    if (n == 0) return m + 1;
+    else if (n != 0 && m == 0) return Accerman(n - 1, 1);
+    else return Accerman(n - 1, Accerman(n, m - 1));
+}
+
+Console.Write("Inpup m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Accerman(m, n);
