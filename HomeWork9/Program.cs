e08a9fa7fa903c8c51d57de620a27bce16b4390b
Anton Tishchenko <AntonTishchenk0@yandex.ru>
2023-08-03 02:41:37 +0200

    Add Task2

--- HomeWork9/Program.cs
+++ HomeWork9/Program.cs
@@ -12,10 +12,20 @@ Console.Write("Input a number: ");
 int numb = Convert.ToInt32(Console.ReadLine());
 OutputNaturalNumbers(numb);*/
 
-// Task 2. Задайте значения M и N. Напишите программу, 
+/* Task 2. Задайте значения M и N. Напишите программу, 
 // которая найдёт сумму натуральных элементов в промежутке от M до N.
 
+int SumNaturalElements(int m, int n)
+{
+    if (m > n) return 0;
+    else return m + SumNaturalElements(m + 1, n);
+}
 
+Console.Write("Input m: ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input n: ");
+int numb2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumNaturalElements(numb1, numb2));*/
 
 // Task 3. Напишите программу вычисления функции Аккермана с помощью рекурсии. 
 // Даны два неотрицательных числа m и n.
